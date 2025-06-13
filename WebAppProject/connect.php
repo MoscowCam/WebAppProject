0c27a479692320c9bf0bd2e46ecdd5bb3b0f9e7f
Cameron Smith <cmanr3@gmail.com>
2025-06-13 18:23:37 +0300

    test again

--- WebAppProject/connect.php
+++ WebAppProject/connect.php
@@ -1,32 +1,4 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Teacher Form</title>
-    <link type="text/css" rel="stylesheet" href="../style.css"> 
-</head>
-<body>
-    <header>
-        <h1>New Teacher Registration</h1>
-    </header>
-    
-    <section>
-        <form action="connect.php" method="post">
-
-        <label for="firstName">First Name</label>
-        <input type="text" id="firstName"name="firstName">
-        
-        <label for="lastName">Last Name</label>
-        <input type="text" id="lastName" name="lastName">
-
-        <label for=""></label>
-        </form>
-    </section>
-    
-</body>
-</html>
 
 
 <?php 
@@ -56,3 +28,33 @@ while($i = mysqli_fetch_assoc($response))
     echo "<hr>";
 }
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Teacher Form</title>
+    <link type="text/css" rel="stylesheet" href="../style.css"> 
+</head>
+<body>
+    <header>
+        <h1>New Teacher Registration</h1>
+    </header>
+    
+    <section>
+        <form action="connect.php" method="post">
+
+        <label for="firstName">First Name</label>
+        <input type="text" id="firstName"name="firstName">
+        
+        <label for="lastName">Last Name</label>
+        <input type="text" id="lastName" name="lastName">
+
+        <label for=""></label>
+        </form>
+    </section>
+    
+</body>
+</html> 
