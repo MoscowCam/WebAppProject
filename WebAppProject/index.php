4877e423db5b93e227f3aa40b83a765c93c2d3ab
Cameron Smith <cmanr3@gmail.com>
2025-06-14 12:54:46 +0300

    updated name

---
+++ WebAppProject/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New User Registration</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <header>
+        <h1>New Teacher Registration</h1>
+    </header>
+    
+    <section>
+        <form action="connect.php" method="post">
+
+        <label for="firstName">First Name</label>
+        <input type="text" id="firstName"name="firstName">
+        
+        <label for="lastName">Last Name</label>
+        <input type="text" id="lastName" name="lastName">
+
+        <label for=""></label>
+        </form>
+    </section>
+    
+</body>
+</html>
