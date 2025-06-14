25e5d4f96cb4b5982b1858130f52d69f1183c25b
Cameron Smith <cmanr3@gmail.com>
2025-06-14 20:59:29 +0300

    test

--- WebAppProject/process.php
+++ WebAppProject/process.php
@@ -1,26 +1,28 @@
 <?php 
-require_once('connect.php')
+require_once('connect.php');
 ?>
 
 
 <?php
+
+
     if(isset($_POST)){
         
-        $firstName      = $_POST['firstName'];
-        $lastName       = $_POST['lastName'];
+        $firstname      = $_POST['firstName'];
+        $lastname       = $_POST['lastName'];
         $email          = $_POST['email'];
-        $phoneNumber    = $_POST['phoneNumber'];
+        $phonenumber    = $_POST['phoneNumber'];
         $password       = shal($_POST['password']);
 
-        $query = "INSERT INTO users(firstname, lastname, email, phonenumber, password )
-        VALUES(?,?,?,?,?)";
-        $stmtinsert = $connect->prepare($query);
-        $result = $stmtinsert->execute([$firstName, $lastName, $email, $phoneNumber, $password]);
-        if($result){
-            echo 'Successfully saved';
-        } else{
-            echo 'There were some errors!';
-        }
+            $query = "INSERT INTO users(firstname, lastname, email, phonenumber, password )
+            VALUES(?,?,?,?,?)";
+            $stmtinsert = $connect->prepare($query);
+            $result = $stmtinsert->execute([$firstname, $lastname, $email, $phonenumber, $password]);
+            if($result){
+                echo 'Successfully saved';
+            } else{
+                echo 'There were some errors!';
+            }
         
 
 
