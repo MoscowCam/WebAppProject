8f0da35560c1bf4f3d4df0c546b57116d8a96b18
Cameron Smith <cmanr3@gmail.com>
2025-06-14 20:01:20 +0300

    big changes

--- WebAppProject/connect.php
+++ WebAppProject/connect.php
@@ -12,49 +12,14 @@ $connect = mysqli_connect(
 
 );
 
-$table_name = "tbl_teachers"; #table name
+$table_name = "users"; #table name
 
 $query = "SELECT * FROM $table_name";
 
 $response = mysqli_query($connect, $query);
 
-echo "<strong>$table_name: </strong>";
-while($i = mysqli_fetch_assoc($response))
-{
-    echo "<p>".$i['id']."</p>";
-    echo "<p>".$i['firstName']."</p>";
-    echo "<p>".$i['lastName']."</p>";
-    echo "<p>".$i['date']."</p>";
-    echo "<hr>";
-}
+
+
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Teacher Form</title>
-    <link type="text/css" rel="stylesheet" href="../style.css"> 
-</head>
-<body>
-    <header>
-        <h1>New Teacher Registration</h1>
-    </header>
-    
-    <section>
-        <form action="connect.php" method="post">
-
-        <label for="firstName">First Name</label>
-        <input type="text" id="firstName"name="firstName">
-        
-        <label for="lastName">Last Name</label>
-        <input type="text" id="lastName" name="lastName">
-
-        <label for=""></label>
-        </form>
-    </section>
-    
-</body>
-</html> 
--- WebAppProject/index.php
+++ WebAppProject/index.php
@@ -1,28 +1,89 @@
+<?php 
+require_once('connect.php')
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New User Registration</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
 <body>
+
+    <?php
+    if(isset($_POST['create'])){
+        echo 'User submitted';
+        $firstName      = $_POST['firstName'];
+        $lastName       = $_POST['lastName'];
+        $email          = $_POST['email'];
+        $phoneNumber    = $_POST['phoneNumber'];
+        $password       = $_POST['password'];
+
+        $query = "INSERT INTO users(firstname, lastname, email, phonenumber, password )
+        VALUES(?,?,?,?,?)";
+        $stmtinsert = $connect->prepare($query);
+        $result = $stmtinsert->execute([$firstName, $lastName, $email, $phoneNumber, $password]);
+        if($result){
+            echo 'Successfully saved';
+        } else{
+            echo 'There were some errors!';
+        }
+        
+
+
+    }
+    ?>
+
     <header>
-        <h1>New Teacher Registration</h1>
+        <h1>New Staff Member Registration</h1>
     </header>
     
     <section>
-        <form action="connect.php" method="post">
+        <form action="index.php" method="post">
+            <div class="container">
 
-        <label for="firstName">First Name</label>
-        <input type="text" id="firstName"name="firstName">
-        
-        <label for="lastName">Last Name</label>
-        <input type="text" id="lastName" name="lastName">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h1>Registration Form:</h1>
+                        <p>Fill out the form with correct information.</p>
+                        <hr class="mb-3">
 
-        <label for=""></label>
+                        <label for="firstName"><b>First Name:</b></label>
+                        <input class="form-control" type="text" id="firstName"name="firstName" required>
+                        
+                        <label for="lastName"><b>Last Name:</b></label>
+                        <input class="form-control" type="text" id="lastName" name="lastName" required>
+
+                        <label for="email"><b>Email:</b></label>
+                        <input class="form-control" type="email" id="email" name="email" required>
+
+                        <label for="phoneNumber"><b>Phone number:</b></label>
+                        <input class="form-control" type="text" id="phoneNumber" name="phoneNumber" required>
+
+                        <label for="password"><b>Password:</b></label>
+                        <input class="form-control" type="password" id="password" name="password" required>
+                        
+                        <hr class="mb-3">
+                        <input class="btn btn-primary" type="submit" name="create" value="Sign up">
+                    </div>
+                </div>
+            </div>
         </form>
     </section>
-    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src=" https://cdn.jsdelivr.net/npm/sweetalert2@11.22.0/dist/sweetalert2.all.min.js "></script>
+    <script type="text/javascript">
+        $(function(){
+            Swal.fire({
+                'title': 'Hello'
+                'type' : 'success'
+            })
+        })
+    </script>
+
+
 </body>
 </html>
