c632ae97709312a01091b170489344d4949e7542
Cameron Smith <cmanr3@gmail.com>
2025-06-15 13:13:34 +0300

    test

--- WebAppProject/userlogin/login.php
+++ WebAppProject/userlogin/login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RIS Admin Login</title>
+    <link rel="stylesheet" href="./userlogin/styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/03386e2b32.js" crossorigin="anonymous"></script>
 
