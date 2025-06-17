acd8a249269833b9e17104c819609fe3001bec32
Cameron Smith <cmanr3@gmail.com>
2025-06-17 21:48:12 +0300

    When  setting up a PDO with docker the host has to be the docker container name Ex: PDO('mysql:host=localhost....') <- Will not work. You have to change it to host=[container name] F***

--- WebAppProject/userlogin/config.php
+++ WebAppProject/userlogin/config.php
@@ -4,7 +4,7 @@ $db_user = 'php_docker';    #database name or username?
 $db_pass = 'password';      #password
 $db_name = 'php_docker';        #table name? 
 
-$db = new PDO('mysql:host=localhost;dbname='. $db_name .';charset=utf8', $db_user, $db_pass);
+$db = new PDO('mysql:host=db;dbname='. $db_name .';charset=utf8', $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
